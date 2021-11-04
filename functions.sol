3c50c4535e0ed2dbe32aff1e47389984b555a435
bcaliva21 <bcaliva21@gmail.com>
2021-11-03 20:53:13 -0700

    Added notes on private functions. Added notes on setting a return value for functions.

--- functions.sol
+++ functions.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.7;
 
 contract FunFunFunctions {
 
+  uint[] numbers;
 // we specify that we want to store the _name parameter in memory
 // some types MUST be stored in memory they are
   // arrays
@@ -16,6 +17,20 @@ contract FunFunFunctions {
     ) public
     {
       // some burger eating logic here
+  }
+
+  // this is a simple private function.
+  // this means only other functiosn within our contract will be able to call this
+
+  // once again, pay attention to the naming convention for private functions
+  function _addToArray(uint _number) private {
+    numbers.push(_number);
+  }
+
+  // if we declare a function that returns something we need to specify that in it's declaration
+  function sayHello() public returns (string memory) {
+    string greeting = "Hello Solidity";
+    return greeting;
   }
   // we invoke functions as if in JS
   eatHamburgers("Bradley", 100);
