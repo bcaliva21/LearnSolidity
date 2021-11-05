696c0e915524e55acd6901f04c7c8d78dfda1669
bcaliva21 <bcaliva21@gmail.com>
2021-11-04 20:30:50 -0700

    Added some resources in the BasicToken.sol file. I paraphrased some of my findings, but not all. Highly recommend following the links to better your own understanding.

--- ShitCoin/BasicToken.sol
+++ ShitCoin/BasicToken.sol
@@ -2,8 +2,42 @@ pragma solidity ^0.8.7;
 
 //---------------LINK_COIN_CONTRACT-------------------------------|
 // STOLEN DIRECTLY FROM ETHERSCAN TO STUDY TOKEN MAKING
+// I have made small modifications to this code to
+// 1. Try to follow naming conventions as I have found in the docs
+// 2. Replaced depreciated modifiers
+// 2a. I have replace constant with view
 //---------------END_RANT-----------------------------------------|
 
+//--------------HELPFUL_LINKS_&_INFO------------------------------|
+//
+// Why SafeMath?
+// https://docs.openzeppelin.com/contracts/2.x/api/math
+//
+//
+// Allowance/Approval?
+// SOF https://stackoverflow.com/questions/48664570/what-approve-and-allowance-methods-are-really-doing-in-erc20-standard
+//
+// assert() VS require()
+// ESE https://ethereum.stackexchange.com/questions/15166/difference-between-require-and-assert-and-the-difference-between-revert-and-thro
+// REQUIRE use cases
+/**
+Validate user inputs
+Validate the response from an external contract
+ie. use require(external.send(amount))
+Validate state conditions prior to executing state changing operations, for example in an owned contract situation
+Generally, you should use require more often,
+Generally, it will be used towards the beginning of a function.
+ */
+ // ASSERT use cases
+ /**
+check for overflow/underflow
+check invariants
+validate contract state after making changes
+avoid conditions which should never, ever be possible.
+Generally, you should use assert less often
+Generally, it will be use towards the end of your function.
+  */
+//-----------------END_LINKS--------------------------------------|
 
 
 /**
@@ -14,31 +48,34 @@ pragma solidity ^0.8.7;
 library SafeMath {
 
 // I see they use assert here...
-    // if I understand this correctly (caveat, I'm an idiot) 
-    // you could use the other options below as well
-    // require(logic)
-    // if(logic) throw;
-    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    // look at the link above for a more indepth answer for require vs assert
+    // my synopsis
+    // assert(false) compiles to 0xfe, which is an invalid opcode
+        // because of this it uses up all remaining gas & reverts all changes
+    // require(false) compiles to 0xfd, which is the REVERT opcode
+        // because of this it will REFUND remaining gas
+    // SEE ABOVE (START LINE 11) FOR LINK & MORE INFO ON ASSERT VS REQUIRE
+    function mul(uint256 a, uint256 b) internal view returns (uint256) {
         uint256 c = a * b;
         assert(a ==0 || c / a == b);
         return c;
     }
 
-    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    function div(uint256 a, uint256 b) internal view returns (uint256) {
         // assert(b > 0); // solidity auto throws an error when dividing by 0
         uint256 c = a / b;
         // assert(a == b * c + a % b); // ???
-        // LINK devs say this there is no case where this doesn't hold... 
+        // LINK devs say this there is no case where this doesn't hold...
         // I believe them...
         return c;
     }
 
-    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    function sub(uint256 a, uint256 b) internal view returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
-    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    function add(uint256 a, uint256 b) internal view returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
         return c;
@@ -53,7 +90,7 @@ library SafeMath {
 
 contract ERC20Basic {
     uint256 public totalSupply;
-    function balanceOf(address _who) constant returns (uint256);
+    function balanceOf(address _who) view returns (uint256);
     function transfer(address _to, uint256 _value) returns (bool);
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 }
@@ -82,7 +119,7 @@ contract ERC677Reveiver {
 
 /**
  * @title Basic token
- * @dev Basic version of StandardToken, with no allowances. 
+ * @dev Basic version of StandardToken, with no allowances.
  */
 
 contract BasicToken is ERC20Basic {
@@ -110,11 +147,11 @@ contract BasicToken is ERC20Basic {
 
     /**
     * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of. 
+    * @param _owner The address to query the the balance of.
     * @return An uint256 representing the amount owned by the passed address.
     */
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) view returns (uint256 balance) {
         // return the balance of the sender
         // ??? I'm curious what heppens if the user inputs an invalid address ???
         return balances[_owner];
@@ -122,4 +159,71 @@ contract BasicToken is ERC20Basic {
 
 }
 
-// WILL CONTINUE THIS TOMORROW
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+
+contract StandardToken is ERC20, BasicToken {
+
+    mapping(address => mapping (address => uint256)) allowed;
+
+        /**
+    * @dev Transfer tokens from one address to another
+    * @param _from address The address which you want to send tokens from
+    * @param _to address The address which you want to transfer to
+    * @param _value uint256 the amount of tokens to be transferred
+    */
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+        var _allowance = allowed[_from][msg.sender];
+
+        // no check is needed here, because sub(_allowance, _value) will throw if this condition is not met
+
+        // subtract the transfer amount, _value, from the _from address
+        balances[_from] = balances[_from].sub(_value);
+        // add _value to the _to address
+        balances[_to] = balances[_to].add(_value);
+        // subtract _value from the allowance
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        // invoking the event to communicate with FE
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+      /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+
+   function approve(address _spender, uint256 _value) returns (bool) {
+       // setting the value allowed for the _spender to use
+       allowed[msg.sender][_spender] = _value;
+       // invoking the event to communicate with FE
+       Approval(msg.sender, _spender, _value);
+       return true;
+   }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+
+   function allowance(address _owner, address _spender) view returns (uint256 _remaining) {
+       return allowed[_owner][_spender];
+   }
+
+       /*
+   * approve should be called when allowed[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   */
+
+}
